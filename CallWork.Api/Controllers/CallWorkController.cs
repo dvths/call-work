6fa240bd21733f5df1b7a6a5a3831b086bb304a9
Tiago Henrique <tiagohs.dev@gmail.com>
2023-01-14 11:37:07 -0300

    feat: implementação do CRUD em memória

--- CallWork.Api/Controllers/CallWorkController.cs
+++ CallWork.Api/Controllers/CallWorkController.cs
@@ -1,3 +1,5 @@
+using CallWork.Api.Models;
+using CallWork.Api.Services.CallWork;
 using CallWork.Contracts.CallWork;
 
 using Microsoft.AspNetCore.Mvc;
@@ -5,29 +7,90 @@ using Microsoft.AspNetCore.Mvc;
 namespace CallWork.Api.Controllers;
 
 [ApiController]
-public class CallWorkController: ControllerBase
+[Route("[controller]")]
+public class CallWorkController : ControllerBase
 {
-    [HttpPost("/callwork")]
+    private readonly ICallWorkService _callWorkService;
+
+    public CallWorkController(ICallWorkService callWorkService)
+    {
+        _callWorkService = callWorkService;
+    }
+
+    [HttpPost]
     public IActionResult CreateCall(CreateCallWorkRequest request)
     {
-        return Ok(request);
+        var callwork = new CallWorkModel(
+            Guid.NewGuid(),
+            request.Title,
+            request.Description,
+            request.StartDateTime,
+            request.EndDateTime,
+            DateTime.UtcNow,
+            request.Subjects,
+            request.Technologies);
+
+        // TODO salvar no banco de dados
+        _callWorkService.CreateNewCallWork(callwork);
+
+        var response = new CreateCallWorkResponse(
+            callwork.Id,
+            callwork.Title,
+            callwork.Description,
+            callwork.StartDateTime,
+            callwork.EndDateTime,
+            callwork.LastModifiedDateTime,
+            callwork.Subjects,
+            callwork.Technologies
+        );
+
+        return CreatedAtAction(
+            actionName: nameof(GetCall),
+            routeValues: new { id = callwork.Id },
+            value: response);
     }
 
-    [HttpGet("/callwork/{id:guid}")]
+    [HttpGet("{id:guid}")]
     public IActionResult GetCall(Guid id)
     {
-        return Ok(id);
+        CallWorkModel callwork = _callWorkService.GetCallById(id);
+
+        var response = new CreateCallWorkResponse(
+            callwork.Id,
+            callwork.Title,
+            callwork.Description,
+            callwork.StartDateTime,
+            callwork.EndDateTime,
+            callwork.LastModifiedDateTime,
+            callwork.Subjects,
+            callwork.Technologies
+        );
+        return Ok(response);
     }
 
-    [HttpPut("/callwork/{id:guid}")]
+    [HttpPut("{id:guid}")]
     public IActionResult UpsertCall(Guid id, UpsertCallWorkRequest request)
     {
-        return Ok(request);
+        var callwork = new CallWorkModel(
+            id,
+            request.Title,
+            request.Description,
+            request.StartDateTime,
+            request.EndDateTime,
+            DateTime.UtcNow,
+            request.Subjects,
+            request.Technologies);
+
+        _callWorkService.UpsertCallWork(callwork);
+
+        // TODO retorna 201 se nenhuma callwork tiver sido criada
+        return NoContent();
     }
 
-    [HttpDelete("/callwork/{id:guid}")]
+    [HttpDelete("{id:guid}")]
     public IActionResult DeleteCall(Guid id)
     {
-        return Ok(id);
+        _callWorkService.DeleteCall(id);
+        return NoContent();
     }
 }
