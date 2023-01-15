dccf61698bc13d1e53558afa92df57ed32a93b1a
Tiago Henrique <tiagohs.dev@gmail.com>
2023-01-14 22:08:40 -0300

    feat: define as validações e as respectivas mensagens de erro

--- CallWork.Api/ServiceErrors/Errors.CallWork.cs
+++ CallWork.Api/ServiceErrors/Errors.CallWork.cs
@@ -6,6 +6,16 @@ public static class Errors
 {
     public static class CallWork
     {
+        public static Error InvalidTitle => Error.Validation(
+            code: "CallWork.InvalidTitle",
+            description: $"Call title must be at least {Models.CallWorkModel.MinTitleLength}" +
+                         $" characters long and at most {Models.CallWorkModel.MaxTitleLength} characters long");
+
+        public static Error InvalidDescription => Error.Validation(
+            code: "CallWork.InvalidDescription",
+            description: $"Call description must be at least {Models.CallWorkModel.MinTitleLength}" +
+                         $" characters long and at most {Models.CallWorkModel.MaxTitleLength} characters long");
+
         public static Error NotFound => Error.NotFound(
             code: "CallWork.NotFound",
             description: "Call not found.");
